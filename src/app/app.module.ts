ffecde754106e81322f280cf04a4e3a11deac92a
Fran <vidotto982@gmail.com>
2022-04-26 12:52:24 -0300

    fix css list menu home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,11 +13,12 @@ import { MatIconModule } from "@angular/material/icon";
 import { RouterModule, Routes } from "@angular/router";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatInputModule } from "@angular/material/input";
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from "@angular/material/card";
-import {AuthGuardService} from "./interceptors/interceptor.service";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
+import { AuthGuardService } from "./interceptors/interceptor.service";
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MenuComponent } from './menu/menu.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -41,9 +42,11 @@ import { MenuComponent } from './menu/menu.component';
     FormsModule,
     MatButtonModule,
     MatCardModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatGridListModule
   ],
   providers: [AuthGuardService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
