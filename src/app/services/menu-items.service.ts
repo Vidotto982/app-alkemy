7379f429dcd05381e52b5a897974eb4865edf8e0
Fran <vidotto982@gmail.com>
2022-04-28 18:28:52 -0300

    fix bug person list platos

--- src/app/services/menu-items.service.ts
+++ src/app/services/menu-items.service.ts
@@ -1,22 +1,31 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {query} from "@angular/animations";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class MenuItemsService {
-
-  readonly url: string = '/api/food/menuItems/search?query=a&apiKey=823d60a4694c49a6ad91a822f6b1ea90'
   res: any;
+  apyKey: string = 'apiKey=823d60a4694c49a6ad91a822f6b1ea90'
+  baseUrl: string = environment.url;
+  private _query: string | undefined;
+  readonly url: any;
+  private fullUrl: string | undefined;
   constructor(private http: HttpClient,
   ) {
 
   }
-  getMenu(): any {
-    let header = new HttpHeaders().set('Type-content', 'application/json').set('Access-Control-Allow-Origin','*').set('Access-Control-Allow-Methods','*')
-    return this.http.get(this.url,{headers: header})
+
+  getMenu(searchName: string): any {
+    const _query = `query=${searchName}`
+    const fullUrl = `${(this.baseUrl)}${(_query)}&${(this.apyKey)}`;
+    const url = fullUrl;
+    let header = new HttpHeaders().set('Type-content', 'application/json').set('Access-Control-Allow-Origin', '*').set('Access-Control-Allow-Methods', '*')
+    return this.http.get(<string>url, {headers: header})
   }
 
   deleteMenu(id: any): Observable<{}> {
